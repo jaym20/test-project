e13d5fe994bfb914eca0b4fa82f32748deb91696
jaym20 <jmmakwana206@gmail.com>
2020-07-17 22:12:21 +0530

    Components Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,21 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import 'hammerjs';
-
+import { MenuComponent } from './menu/menu.component';
+import { MatListModule } from '@angular/material/list';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
